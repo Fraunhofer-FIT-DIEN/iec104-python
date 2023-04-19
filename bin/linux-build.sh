4db3d53b3402f2c626397f6d4cb98f7acacb6a2e
martin.unkel <martin.unkel@fit.fraunhofer.de>
2023-04-19 16:35:17 +0200

    update pip commands

--- bin/linux-build.sh
+++ bin/linux-build.sh
@@ -5,8 +5,7 @@ DIR=$(dirname "$(dirname "$(realpath "$0")")")
 CMD="
   apt-get update ; \
   apt-get install -y --no-install-recommends build-essential cmake ninja-build ; \
-  pip3 install ninja ; \
-  python3 setup.py build bdist_wheel
+  python3 -m pip wheel .
 "
 
 docker run -it --rm -v "$DIR:/opt/c104" -w /opt/c104 python:3.6 /bin/bash -c "$CMD"
--- bin/win-build.bat
+++ bin/win-build.bat
@@ -3,9 +3,9 @@
 REM change working directory to repository directory
 cd /D "%~dp0/.."
 
-py -3.11 ./setup.py bdist_wheel
-py -3.10 ./setup.py bdist_wheel
-py -3.9 ./setup.py bdist_wheel
-py -3.8 ./setup.py bdist_wheel
-py -3.7 ./setup.py bdist_wheel
-py -3.6 ./setup.py bdist_wheel
+py -3.11 -m pip wheel .
+py -3.10 -m pip wheel .
+py -3.9 -m pip wheel .
+py -3.8 -m pip wheel .
+py -3.7 -m pip wheel .
+py -3.6 -m pip wheel .
